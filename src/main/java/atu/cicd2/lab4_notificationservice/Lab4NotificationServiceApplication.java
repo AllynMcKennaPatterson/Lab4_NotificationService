e90ec2ddbae893f07cbb6f674730e7f647877e82
G00392765@atu.ie <G00392765@atu.ie>
2023-10-13 15:57:23 +0100

    Initial commit

---
+++ src/main/java/atu/cicd2/lab4_notificationservice/Lab4NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package atu.cicd2.lab4_notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4NotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4NotificationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/atu/cicd2/lab4_notificationservice/Lab4NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.cicd2.lab4_notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
