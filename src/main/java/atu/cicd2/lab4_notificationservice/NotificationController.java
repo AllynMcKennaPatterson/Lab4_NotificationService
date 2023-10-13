6d88355686c1cfc59613b5ffb043d342923e9892
G00392765@atu.ie <G00392765@atu.ie>
2023-10-13 16:06:28 +0100

    Added PostMapping

--- src/main/java/atu/cicd2/lab4_notificationservice/NotificationController.java
+++ src/main/java/atu/cicd2/lab4_notificationservice/NotificationController.java
@@ -1,7 +1,17 @@
 package atu.cicd2.lab4_notificationservice;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NotificationController {
+    @PostMapping("/notification")
+
+    @ResponseStatus(HttpStatus.CREATED)
+    public String registerUserBody() {
+
+    }
 }
